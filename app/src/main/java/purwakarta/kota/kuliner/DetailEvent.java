4dd7625435221e513305f3b93c4c4557dedf3465
aomcenter <aomcenter>
2017-08-26 14:04:28 +0700

    Add Icon Fitur Blog, Event & Promo

--- app/src/main/java/purwakarta/kota/kuliner/DetailEvent.java
+++ app/src/main/java/purwakarta/kota/kuliner/DetailEvent.java
@@ -2,12 +2,77 @@ package purwakarta.kota.kuliner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.List;
+
+import purwakarta.kota.kuliner.api.ApiBase;
+import purwakarta.kota.kuliner.api.ApiInterface;
+import purwakarta.kota.kuliner.helper.SessionManager;
+import purwakarta.kota.kuliner.model.Event;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailEvent extends AppCompatActivity {
-
+    private SessionManager session;
+    private ImageView imageView;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_event);
+        session = new SessionManager(this);
+        HashMap<String, String> keyid = session.getKeyId();
+        String idwr = keyid.get(SessionManager.KEY_ID);
+        String nama = keyid.get(SessionManager.KEY_NAMA);
+        setTitle(nama);
+        imageView = (ImageView) findViewById(R.id.detail_gambar_event);
+        textView = (TextView) findViewById(R.id.deskripsi_event);
+        getDetailEvent(idwr);
+    }
+
+    private void getDetailEvent(String id) {
+
+
+        ApiInterface apiService = ApiBase.getClient().create(ApiInterface.class);
+
+        Call<List<Event>> mService = apiService.getDetailEvent(id);
+        mService.enqueue(new Callback<List<Event>>() {
+            //Asyncronous Request
+            @Override
+            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+                try {
+
+                    //dapatkan hasil parsing dari method response.body()
+                    List<Event> severWarung = response.body();
+                    if (severWarung != null) {
+
+                        Picasso.with(DetailEvent.this).load("http://kulinerpwk.id/maranggi/" + severWarung.iterator().next().getImage()).error(android.R.drawable.stat_notify_error).fit().into(imageView);
+                        textView.setText(severWarung.iterator().next().getDeskripsi());
+
+                    } else {
+                        Toast.makeText(DetailEvent.this, "Data Kosong", Toast.LENGTH_SHORT).show();
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(DetailEvent.this, "Gagal Total", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Event>> call, Throwable t) {
+                call.cancel();
+                Toast.makeText(DetailEvent.this, "Tidak Dapat Terhubung Ke Server", Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 }
--- app/src/main/java/purwakarta/kota/kuliner/EventActivity.java
+++ app/src/main/java/purwakarta/kota/kuliner/EventActivity.java
@@ -1,5 +1,6 @@
 package purwakarta.kota.kuliner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +13,9 @@ import java.util.List;
 import purwakarta.kota.kuliner.adapter.EventAdapter;
 import purwakarta.kota.kuliner.api.ApiBase;
 import purwakarta.kota.kuliner.api.ApiInterface;
+import purwakarta.kota.kuliner.helper.RecyclerViewClickListener;
+import purwakarta.kota.kuliner.helper.RecyclerViewTouchListener;
+import purwakarta.kota.kuliner.helper.SessionManager;
 import purwakarta.kota.kuliner.model.Event;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,6 +25,8 @@ public class EventActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private EventAdapter mAdapter;
+    private List<Event> eventLis;
+    private SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,7 @@ public class EventActivity extends AppCompatActivity {
         mRecyclerView.setItemViewCacheSize(20);
         mRecyclerView.setDrawingCacheEnabled(true);
         mRecyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
+        session = new SessionManager(this);
         getEventa();
     }
 
@@ -51,8 +58,11 @@ public class EventActivity extends AppCompatActivity {
                     //dapatkan hasil parsing dari method response.body()
                     List<Event> eventList = response.body();
                     if (eventList != null) {
+                        eventLis = eventList;
                         mAdapter = new EventAdapter(EventActivity.this, eventList);
                         mRecyclerView.setAdapter(mAdapter);
+
+                        initClick();
 
                     } else {
                         Toast.makeText(EventActivity.this, "Data Kosong", Toast.LENGTH_SHORT).show();
@@ -72,5 +82,27 @@ public class EventActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void initClick() {
+        mRecyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(EventActivity.this, mRecyclerView, new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                String warung = eventLis.get(position).getJudulevent();
+                String id = eventLis.get(position).getIdevent();
+                //Toast.makeText(getContext(),kategori,Toast.LENGTH_SHORT).show();
+                session.setKeyId(id, warung);
+                Intent intent = new Intent(EventActivity.this, DetailEvent.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+        //Toast.makeText(getContext(),item,Toast.LENGTH_SHORT).show();
+
     }
 }
